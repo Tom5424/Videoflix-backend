7be5185d87f4df61748cb00a96eace11d1f1c7ff
Tom <tom.petri7@gmail.com>
2025-06-17 14:36:09 +0200

    Changed the variable name in the if statemnet from default to value

--- videoflix/utils.py
+++ videoflix/utils.py
@@ -1,5 +1,5 @@
 def convert_string_to_boolean(value, default=False):
     ##### Convert a string to a real boolean 
-    if default is None:
+    if value is None:
         return default
     return str(value).strip().lower() in ("true", "1", "yes", "on")
