0319160d41ad353f28d85b5c821025067938d312
Tom <tom.petri7@gmail.com>
2025-06-17 14:03:23 +0200

    refactor: Added additionally a if statement to the convert_string_to_boolean function to prevent errors. And change the default values by the environment variables "EMAIL_USE_TLS" and "EMAIL_USE_SSL", to strings.

--- videoflix/utils.py
+++ videoflix/utils.py
@@ -1,3 +1,5 @@
-def convert_string_to_boolean(value):
+def convert_string_to_boolean(value, default=False):
     ##### Convert a string to a real boolean 
+    if value is None:
+        return default
     return str(value).strip().lower() in ("true", "1", "yes", "on")
